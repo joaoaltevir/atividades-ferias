d966a85c3e343e2a028d81522fa27fcabf7436f6
Neto <j.altevirneto@gmail.com>
2024-07-22 09:53:10 -0300

    feat: implementado função de inserção de dados

---
+++ index.js
@@ -0,0 +1,19 @@
+//variaveis e importações:
+const prompt = require("prompt-sync")();
+const funcao = require("./function");
+let opcao = 0;
+const ATIVIDADES = []
+
+
+//1 parte: C (criar)
+
+while(opcao != 5){
+    funcao.menu();
+    opcao = +prompt("Digite aqui: ")
+    if(opcao == 1){
+        funcao.adicionar(ATIVIDADES);
+    }else if(opcao == 3){
+        funcao.listar
+    }
+}
+
