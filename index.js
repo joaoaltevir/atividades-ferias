40dd0d9b593458b769dc53ebc1c891a01bd56fe5
Neto <j.altevirneto@gmail.com>
2024-07-22 11:27:11 -0300

    refact: formantando código

--- index.js
+++ index.js
@@ -2,27 +2,25 @@
 const prompt = require("prompt-sync")();
 const funcao = require("./atividades");
 let opcao = 0;
-const ATIVIDADES = []
-
+const ATIVIDADES = [];
 
 //1 parte: C (criar)
 
-while(opcao != 5){
-    funcao.menu();
-    opcao = +prompt("Digite aqui: ")
-    if(opcao == 1){
-        funcao.adicionar(ATIVIDADES);
-    }else if(opcao == 2){
-        funcao.listar(ATIVIDADES);
-        funcao.alterar(ATIVIDADES);
-    }else if(opcao == 3){
-        funcao.listar(ATIVIDADES)
-    }else if(opcao == 4){
-        funcao.listar(ATIVIDADES)
-        let escolhaApagar = +prompt("Informe o indice que deseja apagar: ")
-        funcao.apagar(escolhaApagar,ATIVIDADES)
-    }else{
-        console.log("Opção não é válida!")
-    }
+while (opcao != 5) {
+  funcao.menu();
+  opcao = +prompt("Digite aqui: ");
+  if (opcao == 1) {
+    funcao.adicionar(ATIVIDADES);
+  } else if (opcao == 2) {
+    funcao.listar(ATIVIDADES);
+    funcao.alterar(ATIVIDADES);
+  } else if (opcao == 3) {
+    funcao.listar(ATIVIDADES);
+  } else if (opcao == 4) {
+    funcao.listar(ATIVIDADES);
+    let escolhaApagar = +prompt("Informe o indice que deseja apagar: ");
+    funcao.apagar(escolhaApagar, ATIVIDADES);
+  } else {
+    console.log("Opção não é válida!");
+  }
 }
-
